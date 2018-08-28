e4cd54f31c535c8b321aa0315de9181c069f9240
andreyaksenov <andrey.aksenov@jetbrains.com>
2018-08-28 12:22:12 +0300

    add worker_timeout for debugging

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,7 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+worker_timeout 1000000
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
